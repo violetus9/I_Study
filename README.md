f04091e0a7970ef5006dd4a877c24c6355be3452
YSN <greeobedi91@gmail.com>
2021-05-04 23:33:30 +0900

    readme수정, password암호화 적용

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# 오류 기록    
+# 오류 기록   
+###### 에러 입력할 때 ctrl+f 로 검색 되게끔 에러메세지 키워드 써주기
 ***
-## node 경로
+## npm cmdlet
 ###### 깃배시에서 npm실행할 때 오류가 출력됐음
 
 <img src="./img/error_node_path.PNG" width="70%" height="auto" title="error" alt="errorNodePath"></img>    
-경로문제임을 알 수 있다 >> 환경변수에 경로를 추가하자!    
+경로문제 >> 환경변수에 경로를 추가하자!    
 ***
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 // 몽구스 모듈 가져와여
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+// 비크립트 가져와요
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 // 스키마를 생성할게요
 const userSchema = mongoose.Schema({
@@ -31,6 +34,27 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number
   }
+})
+
+// 모델을 저장하기 전에 암호화를 넣어줄것이다
+userSchema.pre('save', function (next) {
+  // 비번 가져오기위해서 객체가져오기
+  let user = this;
+  // 다른 정보 들어와도 비번바뀔때만 암호화가 동작하게끔
+  if (user.isModified('password')) {
+    // 비밀번호를 암호화 시킨다.(bcrypt npm 가서 떠오삼)
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      // next하면 레지스트라우터로 바로간댔지 > 에러나면 바로 보내자
+      if (err) return next(err)
+      bcrypt.hash(user.password, salt, function (err, hash) { // hash가 암호화된 비번임
+        // Store hash in your password DB.
+        if (err) return next(err)
+        user.password = hash
+        next()
+      });
+    });
+  }
+
 })
 
 // 스키마를 모델로 감싼다고 햇죠
