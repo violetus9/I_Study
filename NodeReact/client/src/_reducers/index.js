5003ebc156736e34395c8d085ccfda4f92a37429
YSN <greeobedi91@gmail.com>
2021-05-13 23:13:00 +0900

    add redux(reducer)

---
+++ NodeReact/client/src/_reducers/index.js
@@ -0,0 +1,14 @@
+// combineReducer : 스토어에 리듀서 여러개 있을 수 있음(리듀서 안에서 하는 일이 어떻게 state가 변하는지 보여준담에
+// 마지막 값을 리턴하는게 리듀서인데, 유저, 구독, 코멘트 여러개 state있을 수 있으니 reducer나눠져있을 수 있어)
+// combine>> root reducer서 하나로 합쳐줌
+// 로그인이나 레지, 코멘트 이런거 만들거임
+// 기능 많아질수록 rootReducer가 길어지겠쬬
+import { combineReducers } from 'redux';
+// import user from './user_reducer';
+
+const rootReducer = combineReducers({
+  // user
+})
+
+// 다른 폴더서 쓸 수 있게 디폴트
+export default rootReducer;
--- NodeReact/client/src/index.js
+++ NodeReact/client/src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+// 요케만 해도 index.js 처리해줌
+import Reducer from './_reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// 원래는 createStore만해서 store를 redux서 생성하는건데
+// store 객체만 받는다니까 promise랑 thunk넣어서 다른것도 받을 수 있게 해줌
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
+ReactDOM.render(
+
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
