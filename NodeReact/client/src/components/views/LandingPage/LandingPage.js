87c2aa1d6eb47af79b3b3921d3cc0a2d5f53f3a5
YSN <greeobedi91@gmail.com>
2021-05-16 02:27:06 +0900

    add logout

--- NodeReact/client/src/components/views/LandingPage/LandingPage.js
+++ NodeReact/client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,23 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   // 랜딩페이지 오자마자 이걸 실행할건데, get > req를 서버로 보낼것, /api/hello로
   useEffect(() => {
     axios.get('/api/hello')
       .then(response => console.log(response.data))
   }, [])
 
-
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`)
+      .then(response => {
+        if (response.data.success) {
+          props.history.push('/login')
+        } else {
+          alert('failed logout !')
+        }
+      })
+  }
 
   return (
     <div style={{
@@ -16,6 +25,13 @@ function LandingPage() {
       , width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+
+        로그아웃
+
+      </button>
+
     </div>
   )
 }
