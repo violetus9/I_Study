16cfaf5e5668523ea772c4afb1f587eb315711d8
YSN <greeobedi91@gmail.com>
2021-05-19 14:31:14 +0900

    add uploadForm(1/2)

--- NodeReact_youtube/boilerplate-mern-stack-master/README.md
+++ NodeReact_youtube/boilerplate-mern-stack-master/README.md
@@ -1,7 +1,7 @@
 # 따라하며 배우는 노드, 리액트 시리즈 - 유튜브
 
 
-To use this application, 
+To use this bioler-plate, 
 
 1. make dev.js file inside config folder 
 2. put mongoDB info into dev.js file 
---
+++ NodeReact_youtube/boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import Dropzone from 'react-dropzone';
+
+const { TextArea } = Input;
+const { Title } = Typography;
+
+function VideoUploadPage() {
+
+  // state 만들거, state에 value들 저장할거임, 이걸 또 서버로 한번에 보낼거임
+  const [VideoTitle, setVideoTitle] = useState("")
+  const [Description, setDescription] = useState("")
+  const [Private, setPrivate] = useState(0)
+  const [Category, setCategory] = useState('Film & Animation')
+
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+
+      <Form onSubmit>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {/* <Dropzone
+            onDrop
+            multiple
+            maxSize
+          >
+            maxSize{({ getRootProps, getInputProps }) => (
+              <div style={{
+                width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex',
+                alignItems: 'center', justifyContent: 'center'
+              }} {...getRootProps()}>
+                <input {...getInputProps()} />
+                <Icon type="plus" style={{ fontSize: '3rem' }} />
+              </div>
+            )}
+          </Dropzone> */}
+
+          <div>
+            <img src alt />
+          </div>
+
+        </div>
+
+        <br />
+        <br />
+        <label>Title</label>
+        <Input
+          onChange
+          value={VideoTitle} />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea
+          onChange
+          value={Description}
+        />
+        <br />
+        <br />
+
+        <select onChange>
+          <option key value></option>
+        </select>
+
+
+        <select onChange>
+          <option key value></option>
+        </select>
+        <br />
+        <br />
+
+        <Button type="primary" size="large" onClick>
+          Submit
+        </Button>
+
+      </Form>
+
+
+    </div>
+  )
+}
+
+export default VideoUploadPage
