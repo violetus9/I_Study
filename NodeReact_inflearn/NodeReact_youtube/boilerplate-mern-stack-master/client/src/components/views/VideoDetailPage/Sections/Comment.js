05f897730f5309a2062d97d12f20a732965d5ad9
YSN <greeobedi91@gmail.com>
2021-05-29 12:38:47 +0900

    add comment!

--- NodeReact_inflearn/NodeReact_youtube/boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/Sections/Comment.js
+++ NodeReact_inflearn/NodeReact_youtube/boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Axios from 'axios'
 import { useSelector } from 'react-redux'
 import SingleComment from './SingleComment'
+import ReplyComment from './ReplyComment'
 
 
 function Comment(props) {
@@ -50,7 +51,10 @@ function Comment(props) {
 
       {props.commentLists && props.commentLists.map((comment, index) => {
         (!comment.responseTo &&
-          <SingleComment refreshFunction={refreshFunction} comment={comment} postId={videoId} />
+          <React.Fragment>
+            <SingleComment refreshFunction={props.refreshFunction} comment={comment} postId={videoId} />
+            <ReplyComment refreshFunction={props.refreshFunction} parentCommentId={comment._id} postId={videoId} commentLists={props.commentLists} />
+          </React.Fragment>
         )
       })}
 
