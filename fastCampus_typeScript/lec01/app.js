0f3a1b6dbbb8fe0d2d078631646c799557fbeb5f
YSN <greeobedi91@gmail.com>
2021-06-04 17:48:12 +0900

    'checkPoint'

--- fastCampus_typeScript/lec01/app.js
+++ fastCampus_typeScript/lec01/app.js
@@ -1,7 +1,11 @@
+// 중복이 되는 요소는 유지보수에 유용하지 못하다, 묶어주자
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-
+const content = document.createElement('div');
 // 변경의 여지가 있는 것은 변수로 빼두는 것이 좋다
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 ajax.open('GET', NEWS_URL, false);
 
@@ -13,10 +17,30 @@ const newsFeed = JSON.parse(ajax.response);
 
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function () {
+  const id = location.hash.substr(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement('h1');
+
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+  console.log(newsContent);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement('a');
+
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  a.addEventListener('click', function () { });
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
--- fastCampus_typeScript/lec01/dist/app.c328ef1a.js
+++ fastCampus_typeScript/lec01/dist/app.c328ef1a.js
@@ -118,24 +118,43 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-var ajax = new XMLHttpRequest(); // 변경의 여지가 있는 것은 변수로 빼두는 것이 좋다
+// 중복이 되는 요소는 유지보수에 유용하지 못하다, 묶어주자
+var container = document.getElementById('root');
+var ajax = new XMLHttpRequest();
+var content = document.createElement('div'); // 변경의 여지가 있는 것은 변수로 빼두는 것이 좋다
 
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 ajax.open('GET', NEWS_URL, false); // data 가져옴
 
 ajax.send(); // data 처리(response to Object)
 
 var newsFeed = JSON.parse(ajax.response);
 var ul = document.createElement('ul');
+window.addEventListener('hashchange', function () {
+  var id = location.hash.substr(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+  var newsContent = JSON.parse(ajax.response);
+  var title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+  console.log(newsContent);
+});
 
 for (var i = 0; i < 10; i++) {
   var li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title;
+  var a = document.createElement('a');
+  a.href = "#".concat(newsFeed[i].id);
+  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
+  a.addEventListener('click', function () {});
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
-},{}],"../../../Users/all98/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+container.appendChild(ul);
+container.appendChild(content);
+},{}],"../../../../Users/all98/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -163,7 +182,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14094" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2521" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -339,5 +358,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../Users/all98/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
+},{}]},{},["../../../../Users/all98/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
 //# sourceMappingURL=/app.c328ef1a.js.map
